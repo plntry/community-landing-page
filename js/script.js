0b5f0f73a5bb323f30e3f38de5a28e4f0bf32db0
plntry <nedilko7b@gmail.com>
2025-01-14 21:30:13 +0200

    added hero section & navbar scroll bg update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mobileMenuToggleBtn = document.querySelector('.navbar__mobile-menu-action--toggle-menu');
-  const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
-  console.log(mobileMenuToggleBtn, mobileMenu);
-  
+  // Toggle Theme
+  const themeToggleBtns = document.querySelectorAll(
+    ".navbar__menu-theme, .navbar__mobile-menu-action--toggle-theme"
+  );
+  const body = document.body;
+
+  themeToggleBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      body.classList.toggle("body--theme-dark");
+    });
+  });
+
+  // Toggle Mobile Menu
+  const mobileMenuToggleBtn = document.querySelector(
+    ".navbar__mobile-menu-action--toggle-menu"
+  );
+  const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
 
   mobileMenuToggleBtn.addEventListener("click", function () {
-    console.log('test');
-    
+    console.log("test");
+
     mobileMenu.classList.toggle("navbar__mobile-menu-items--active");
   });
+});
+
+// Change navbar bg color on scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar--scroll");
+  } else {
+    navbar.classList.remove("navbar--scroll");
+  }
 });
